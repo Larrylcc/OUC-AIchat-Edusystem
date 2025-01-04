76e0d6de578a39b47af5f62ada09465f8c156a22
Lu Chen <110882079+Larrylcc@users.noreply.github.com>
2025-01-04 13:23:36 +0800

    Initial Commit

---
+++ miniprogram/envList.js
@@ -0,0 +1,6 @@
+const envList = [{"envId":"network-exper-1gk8awfm2865238c","alias":"network-exper"},{"envId":"edusystem-test-3g4jmh2ze18ac322","alias":"edusystem-test"}]
+const isMac = true
+module.exports = {
+    envList,
+    isMac
+}
